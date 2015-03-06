33280060e497f32362a8adcc13d52c985ae1cebd
alexander_fuchs <alexander_fuchs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-06 09:07:39 +0000

    fixed a bug, where the editor would crash(PHP code shortened after page save). All the code you enter is now properly escaped. Thanks sternhagel for the bug report.
    git-svn-id: https://plugins.svn.wordpress.org/php-everywhere/trunk@1106655 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options_box.php
+++ options_box.php
@@ -7,6 +7,7 @@ function php_everywhere_options_box_content( $post_id ) {
     // nonce field for all your meta boxes of same plugin
     wp_nonce_field( plugin_basename( __FILE__ ), 'php_everywhere' );
 	$text = get_post_meta( get_the_ID(),'php_everywhere_code',true);
+	$text = htmlspecialchars($text);
 	if($text == '')
 	{
 		$text = 'Just put [php_everywhere] where you want the code to be executed.';
@@ -31,7 +32,7 @@ function php_everywhere_data() {
     // for example particular user, role or maybe post type in case of custom post types
 	$post_id = get_the_ID();
     // now store data in custom fields based on checkboxes selected
-    update_post_meta( $post_id, 'php_everywhere_code', $_POST['php_everywhere_code'] );
+    update_post_meta( $post_id, 'php_everywhere_code', htmlspecialchars_decode($_POST['php_everywhere_code']) );
 	update_post_meta(76, 'my_key', $post_id);
 }
 ?>
